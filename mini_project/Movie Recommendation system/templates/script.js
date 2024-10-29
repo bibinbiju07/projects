906f044fa15230dc61c18f199a8b56f3865b4b06
bibinbiju07 <151710874+bibinbiju07@users.noreply.github.com>
2024-10-29 22:29:18 +0530

    Delete mini_project directory

--- mini_project/Movie Recommendation system/templates/script.js
+++
@@ -1,6 +0,0 @@
-function searchMovies() {
-    const query = document.getElementById('movie-search').value;
-    // Implement the search functionality here
-    // For now, let's just log the search query
-    console.log('Searching for:', query);
-}

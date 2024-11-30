53ccb485de837d74f4c5b1bfc822adc619f21a7f
bibinbiju07 <151710874+bibinbiju07@users.noreply.github.com>
2024-11-30 13:37:14 +0530

    Add files via upload

---
+++ Realestate Management System/database/realestate_db.sql
@@ -0,0 +1,227 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.7.9 : Database - realestate
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`realestate` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `realestate`;
+
+/*Table structure for table `advertisement` */
+
+DROP TABLE IF EXISTS `advertisement`;
+
+CREATE TABLE `advertisement` (
+  `ad_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `property_id` int(11) DEFAULT NULL,
+  `ad_name` varchar(50) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`ad_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `advertisement` */
+
+/*Table structure for table `book_details` */
+
+DROP TABLE IF EXISTS `book_details`;
+
+CREATE TABLE `book_details` (
+  `bd_id` int(11) NOT NULL AUTO_INCREMENT,
+  `bm_id` int(11) DEFAULT NULL,
+  `property_id` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `total_amount` varchar(50) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`bd_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `book_details` */
+
+/*Table structure for table `book_master` */
+
+DROP TABLE IF EXISTS `book_master`;
+
+CREATE TABLE `book_master` (
+  `bm_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) DEFAULT NULL,
+  `total_amount` varchar(50) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`bm_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `book_master` */
+
+/*Table structure for table `booking` */
+
+DROP TABLE IF EXISTS `booking`;
+
+CREATE TABLE `booking` (
+  `booking_id` int(11) NOT NULL AUTO_INCREMENT,
+  `property_id` int(11) DEFAULT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`booking_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `booking` */
+
+insert  into `booking`(`booking_id`,`property_id`,`customer_id`,`date`,`status`) values (2,1,2,'2022-01-18','Paid');
+
+/*Table structure for table `category` */
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `cat_name` varchar(50) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `category` */
+
+insert  into `category`(`category_id`,`cat_name`,`description`) values (1,'Flat','1BH'),(2,'Home','Good');
+
+/*Table structure for table `customer` */
+
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `login_id` int(11) DEFAULT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `gender` varchar(50) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `place` varchar(50) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `pincode` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `customer` */
+
+insert  into `customer`(`customer_id`,`login_id`,`firstname`,`lastname`,`gender`,`address`,`place`,`phone`,`email`,`pincode`) values (2,6,'Blesson','Baby','male','Modiyil','Kuttanad','9656323075','blesson@gmail.com','690509');
+
+/*Table structure for table `feedback` */
+
+DROP TABLE IF EXISTS `feedback`;
+
+CREATE TABLE `feedback` (
+  `feedback_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) DEFAULT NULL,
+  `feedback` varchar(200) DEFAULT NULL,
+  `reply` varchar(50) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`feedback_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `feedback` */
+
+insert  into `feedback`(`feedback_id`,`customer_id`,`feedback`,`reply`,`date`) values (1,1,'Hello','pending','2022-01-18'),(2,2,'Hii','pending','2022-01-18');
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `login_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `usertype` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`login_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+/*Data for the table `login` */
+
+insert  into `login`(`login_id`,`username`,`password`,`usertype`) values (1,'admin','admin','admin'),(4,'baby','baby','seller'),(5,'susan','susan','seller'),(6,'bless','blesson','customer');
+
+/*Table structure for table `payment` */
+
+DROP TABLE IF EXISTS `payment`;
+
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `booking_id` int(11) DEFAULT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `total_amount` varchar(50) DEFAULT NULL,
+  `pay_status` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`payment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `payment` */
+
+/*Table structure for table `properties` */
+
+DROP TABLE IF EXISTS `properties`;
+
+CREATE TABLE `properties` (
+  `property_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `seller_id` int(11) DEFAULT NULL,
+  `property_image` varchar(500) DEFAULT NULL,
+  `amount` varchar(50) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`property_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `properties` */
+
+insert  into `properties`(`property_id`,`category_id`,`seller_id`,`property_image`,`amount`,`description`,`status`) values (1,1,1,'static/uploads/a3849dfd-ef18-4b8b-9c48-9e07536eb09bIMG-20210313-WA0004.jpg','100000000','         Good               \r\n                    ','Rent'),(2,2,1,'static/uploads/2db375e2-3516-4579-bbd0-8b11d7507105IMG-20210313-WA0004.jpg','100000000','                        \r\n                    Good','Buy');
+
+/*Table structure for table `seller` */
+
+DROP TABLE IF EXISTS `seller`;
+
+CREATE TABLE `seller` (
+  `seller_id` int(11) NOT NULL AUTO_INCREMENT,
+  `login_id` int(11) DEFAULT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `gender` varchar(50) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `place` varchar(50) DEFAULT NULL,
+  `pincode` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`seller_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `seller` */
+
+insert  into `seller`(`seller_id`,`login_id`,`firstname`,`lastname`,`gender`,`address`,`phone`,`email`,`place`,`pincode`) values (1,4,'Baby','Daniel','male','Modiyil','9544027171','baby@gmail.com','Kuttanad','690890'),(2,5,'Susan','Baby','female','Modiyil','9078675621','susan@gmail.com','Chengannur','690509');
+
+/*Table structure for table `state` */
+
+DROP TABLE IF EXISTS `state`;
+
+CREATE TABLE `state` (
+  `state_id` int(11) NOT NULL AUTO_INCREMENT,
+  `state` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`state_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `state` */
+
+insert  into `state`(`state_id`,`state`,`country`,`city`) values (1,'Kerala','India','Alappuzha'),(2,'Tamilnadu','India','Chennai');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
